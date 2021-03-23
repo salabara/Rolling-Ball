a31add9539c6d7f0b468b7b63f62f733a7dae5f0
salabara <kids246@gmail.com>
2021-03-23 13:05:21 -0400

    Add files via upload

---
+++ Assets/Scripts/UIScale.cs
@@ -0,0 +1,29 @@
+﻿//Bo-Chen Kuo authors
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScale : MonoBehaviour {
+    public float dispalcementX = -0.33f;
+    public float dispalcementY = 0.33f;
+    public float scale = 1f;
+
+    // Start is called before the first frame update
+    void Start () {
+        setScale ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        setScale ();
+    }
+
+    void setScale () {
+        transform.localPosition = new Vector3 (Screen.width * dispalcementX, Screen.height * dispalcementY, 0);
+        transform.localScale = new Vector3 (min (Screen.width / 1920f * scale, Screen.height / 1080f * scale), min (Screen.width / 1920f * scale, Screen.height / 1080f * scale), 1);
+    }
+
+    float min (float a, float b) {
+        return a < b ? a : b;
+    }
+}
